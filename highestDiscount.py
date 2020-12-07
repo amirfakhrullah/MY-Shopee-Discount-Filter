99ffc61a92664d79449be21bae63e90b5136b036
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2020-12-07 15:39:59 +0800

    to find the highest dicount

---
+++ highestDiscount.py
@@ -0,0 +1,11 @@
+
+def highest_discount(prod_list):
+    max_discount = 0
+    max_index = -1
+    for i in range(len(prod_list)):
+        item = prod_list[i]
+        curr_discount = int(item['Discount'].strip('%'))
+        max_discount = max(curr_discount, max_discount)
+        if max_discount == curr_discount:
+            max_index = i
+    return prod_list[max_index]
