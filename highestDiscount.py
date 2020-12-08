5f5bf83d046e03699474fcff8c1f97fa0325f300
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2020-12-08 08:52:21 +0800

    to find the best offer

--- highestDiscount.py
+++ highestDiscount.py
@@ -2,12 +2,13 @@
 def highest_discount(prod_list):
     max_discount = 0
     max_index = -1
-    
     for i in range(len(prod_list)):
         item = prod_list[i]
         curr_discount = int(item['Discount'].strip('%'))
         max_discount = max(curr_discount, max_discount)
         if max_discount == curr_discount:
             max_index = i
-            
-    return prod_list[max_index]
+    try:
+        return prod_list[max_index]
+    except:
+        return 'No best offer found!'
