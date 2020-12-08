d69bff467c441276e7159b74cbc459a62027041b
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2020-12-08 14:05:12 +0800

    bug fix

--- categorize.py
+++ categorize.py
@@ -1,10 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC 
-from bs4 import BeautifulSoup
-
-
 def product_with_discount(discount):
     prod = {
         'Discount': '',
