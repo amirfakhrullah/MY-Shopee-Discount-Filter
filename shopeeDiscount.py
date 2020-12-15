100b9f480f50d0df5430c3f01997e7f8226f20e2
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2020-12-15 16:47:25 +0800

    bugs fixed

--- shopeeDiscount.py
+++ shopeeDiscount.py
@@ -12,8 +12,8 @@ from highestDiscount import highest_discount
 
 search = str(input("What are you searching for?\n:"))
 print('searching ...')
-searchurl = search.replace(' ', '%20')
-searchsplit = search.split()
+searchurl = search.replace(' ', '%20') # for the url
+searchsplit = search.split() # for later use
 url = 'https://shopee.com.my/search?keyword={}&page=0'.format(searchurl)
 
 driver = webdriver.Chrome()
@@ -24,8 +24,10 @@ try:
 except:
     Exception()
 
+# to loop between pages
 page = 0
 while page != 5:
+    # shopee page loads a bit slow, products in the page sometimes unidentified, so I have to implement time.sleep(),implicitly_wait() and execute_script() to scroll the page(to load)
     driver.implicitly_wait(3)
     #driver.execute_script("window.scrollTo(0,(document.body.scrollHeight)/4)")
     #time.sleep(3)
@@ -47,33 +49,37 @@ while page != 5:
 
     print(str(len(products)) + ' products on this page...')
     prod_list = []
+    
+    # to loop between products shown in that page
     for product in products:
         discount = ''
         try:
             discount = product.find('span', {'class':'percent'}).text
-            prod = product_with_discount(discount)
+            prod = product_with_discount(discount) #if the product has a discount, save the product in the dictionary product_with_discount
         except:
             Exception()
     
         if len(discount) > 0:
             try:
                 name = product.find('div', {'class':'_1NoI8_ _1JBBaM'})
-                prod['Name'] = name.text
+                prod['Name'] = name.text  # store the product's name
             except:
                 Exception()
             try:
                 name = product.find('div', {'class':'_1NoI8_ _16BAGk'})
-                prod['Name'] = name.text
+                prod['Name'] = name.text # there are 2 class names for the product name
             except:
                 Exception()
         
             right_item = True
             for word in searchsplit:
+                # check again if the product search has the same word as the current product's name
                 if word.lower() not in prod['Name'].lower():
                     right_item = False
             if right_item == False: break
 
             prices = product.find_all('span', {'class':'_341bF0'})
+            # to store the price range
             if len(prices) == 2:
                 prod['Price_lowest'] = 'RM' + prices[0].text
                 prod['Price_highest'] = 'RM' + prices[1].text
@@ -82,10 +88,11 @@ while page != 5:
         
             try:
                 link = product.find('a', href=True)
-                prod['Link'] = link['href']
+                prod['Link'] = link['href'] # store the href link of the product
                 prod_list.append(prod)
             except:
                 Exception()
+    # to move to the next page
     page += 1
     print('page ' + str(page))
     if page != 5:
@@ -98,9 +105,9 @@ while page != 5:
 print(str(len(prod_list)) + ' products found with discounts')
 print('The best offer is...')
 
-buy_now = highest_discount(prod_list)
-pprint.pprint(buy_now)
+buy_now = highest_discount(prod_list) # highest_discount is to find the product with the most discount
+pprint.pprint(buy_now) # pprint is to print the dictionary in a cleaner look
 try:
-    driver.get('https://shopee.com.my/' + buy_now['Link'])
+    driver.get('https://shopee.com.my/' + buy_now['Link']) # to browse the page of the product with the most discount
 except:
     Exception()
